8946a75637986840815712cc5cd638544b2e490a
Jackson Barker <jcrb45@hotmail.com>
2024-04-29 12:35:50 -0500

    added slices from redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import { Fragment } from "react";
+import Header from "./components/Header.js";
+import Auth from "./components/Auth.js";
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Header></Header>
+      <Auth></Auth>
+      <Counter />
+    </>
   );
 }
 
